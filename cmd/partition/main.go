220178b599acd9e6372b15b2e5fe107bc9a0bb7f
Kamell Perry <kamellperry33@gmail.com>
2026-01-08 09:11:44 -0600

    feat: implement partitioner

--- cmd/partition/main.go
+++ cmd/partition/main.go
@@ -1,7 +1,55 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"os"
+	"strings"
+)
+
+const (
+	inputPath  = "dist/profiles.txt"
+	outputPath = "dist/scrape_data.json"
+)
+
+type Profile struct {
+	Username   string `json:"username"`
+	ProfileURL string `json:"profileURL"`
+}
 
 func main() {
-	fmt.Println("Hello, World!")
+	var (
+		profiles []Profile
+	)
+	data, err := os.ReadFile(inputPath)
+	if err != nil {
+		log.Fatalf("input file not found at %s: %v", inputPath, err)
+		return
+	}
+
+	for line := range strings.Lines(string(data)) {
+		s := strings.SplitAfter(line, "/")
+
+		username := s[len(s)-1]
+		url := line
+
+		p := Profile{
+			Username:   username,
+			ProfileURL: url,
+		}
+
+		profiles = append(profiles, p)
+	}
+
+	out, err := os.Create(outputPath)
+	if err != nil {
+		log.Fatalf("input file not found at %s: %v", inputPath, err)
+		return
+	}
+	defer out.Close()
+
+	if err := json.NewEncoder(out).Encode(&profiles); err != nil {
+		log.Fatalf("failed to decode json: %v", err)
+		return
+	}
 }
