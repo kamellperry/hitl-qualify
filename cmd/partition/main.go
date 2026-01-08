5495374a1997dbca3bba751933ac89d89d25d48e
Kamell Perry <kamellperry33@gmail.com>
2026-01-08 09:49:40 -0600

    fix: partition appends json data not overwrite

--- cmd/partition/main.go
+++ cmd/partition/main.go
@@ -30,8 +30,8 @@ func main() {
 	for line := range strings.Lines(string(data)) {
 		s := strings.SplitAfter(line, "/")
 
-		username := s[len(s)-1]
-		url := line
+		username := strings.TrimSpace(s[len(s)-1])
+		url := strings.TrimSpace(line)
 
 		p := Profile{
 			Username:   username,
@@ -41,14 +41,48 @@ func main() {
 		profiles = append(profiles, p)
 	}
 
-	out, err := os.Create(outputPath)
+	outInfo, err := os.Stat(outputPath)
 	if err != nil {
 		log.Fatalf("input file not found at %s: %v", inputPath, err)
 		return
 	}
-	defer out.Close()
 
-	if err := json.NewEncoder(out).Encode(&profiles); err != nil {
+	if outInfo.Size() < 1 {
+		// File doesn't contain any data, just write to it
+		out, err := os.Create(outputPath)
+		if err != nil {
+			log.Fatalf("failed to create file %s: %v", inputPath, err)
+			return
+		}
+		defer out.Close()
+
+		writeJSON(out, &profiles)
+	}
+
+	// Append data
+	p, err := os.ReadFile(outputPath)
+	if err != nil {
+		log.Fatalf("failed to read file: %v", err)
+	}
+
+	var currentData []Profile
+	if err := json.Unmarshal(p, &currentData); err != nil {
+		log.Fatalf("failed to unmarshal data: %v", err)
+	}
+
+	currentData = append(currentData, profiles...)
+
+	f, err := os.Create(outputPath)
+	if err != nil {
+		log.Fatalf("failed to create file %s: %v", inputPath, err)
+		return
+	}
+	defer f.Close()
+	writeJSON(f, &currentData)
+}
+
+func writeJSON(file *os.File, data *[]Profile) {
+	if err := json.NewEncoder(file).Encode(data); err != nil {
 		log.Fatalf("failed to decode json: %v", err)
 		return
 	}
